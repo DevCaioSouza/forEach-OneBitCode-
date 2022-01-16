34085d90d2749dd439f903d5d531b79d14bd83d9
Caio Souza <84105396+manoCaioZ@users.noreply.github.com>
2022-01-16 23:23:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+numeros = [1,2,3,4,5];
+
+// for(i = 0; i < numeros.length; i++){
+//   console.log(numeros[i])
+// };
+
+// numeros.forEach(function(numero){
+//   console.log(numero);
+// })
+
+numeros.forEach(numeros => console.log(numeros))
